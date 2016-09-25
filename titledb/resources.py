3cb7671738e61c689e3f9dab20832640908a0e62
Ken Sanislo <ken@intherack.com>
2016-09-24 21:45:43 -0700

    group:mod

--- titledb/resources.py
+++ titledb/resources.py
@@ -4,7 +4,7 @@ from pyramid.security import Allow, Everyone
 class Root(object):
     __acl__ = [
                (Allow, Everyone, 'view'),
-               (Allow, 'group:editors', 'edit'),
+               (Allow, 'group:mod', 'edit'),
                (Allow, 'group:super', 'super'),
               ]
 
