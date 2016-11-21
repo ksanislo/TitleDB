a6534838df23c2e14f128be3bb612e1827a01661
Ken Sanislo <ken@intherack.com>
2016-11-20 20:29:02 -0800

    basic CIA adding

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 requires = [
     'pyramid',
     'pyramid_chameleon',
+    'numpy',
     'marshmallow',
     'deform',
     'sqlalchemy',
--- titledb/magic.py
+++ titledb/magic.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import requests
 import hashlib
 import transaction
@@ -5,6 +8,8 @@ import json
 import os
 import re
 import mimetypes
+import numpy
+import base64
 
 from datetime import datetime
 
@@ -12,12 +17,17 @@ from .models import (
     DBSession,
     URL,
     URLSchema,
+    ARM9,
+    CIA,
+    SMDH,
+    TDSX
 )
 
 mimetypes.add_type('application/x-3ds-archive','.cia')
 mimetypes.add_type('application/x-3ds-homebrew','.3dsx')
 mimetypes.add_type('application/x-3ds-iconfile','.smdh')
 mimetypes.add_type('application/x-3ds-arm9bin','.bin')
+mimetypes.add_type('application/x-3ds-xml','.xml')
 
 def checksum_sha256(filename):
     h = hashlib.sha256()
@@ -72,15 +82,15 @@ def download_to_filename(r, filename):
 
         return(calculated_size, h.hexdigest())
 
-def process_url(url=None, url_id=None, cache_root=''):
+def process_url(url_string=None, url_id=None, cache_root=''):
     with transaction.manager:
         new = False
-        if url:
-            url = url.split('#')[0]    # Remove any # target from the URL
-            item = DBSession.query(URL).filter_by(url=url).first()
+        if url_string:
+            url_string = url_string.split('#')[0]    # Remove any # target from the URL
+            item = DBSession.query(URL).filter_by(url=url_string).first()
             if not item:
                 new = True
-                item = URL(url=url)
+                item = URL(url=url_string)
         elif url_id:
             item = DBSession.query(URL).get(url_id)
             if not item:
@@ -139,8 +149,23 @@ def process_url(url=None, url_id=None, cache_root=''):
             if not item.size or not item.sha256:
                 None # TODO: Errors happened during download.
 
+            # FIXME: This would be cleaner as a function.
+            switcher = {
+                'application/x-3ds-archive': add_cia,
+                'application/x-3ds-homebrew': add_tdsx,
+                'application/x-3ds-iconfile': add_smdh,
+                'application/x-3ds-arm9bin': add_arm9,
+                'application/x-3ds-xml': add_xml
+            }
+            action = switcher.get(item.content_type, None)
+            if action:
+                item.active = action(item, cache_path=cache_path)
+            else:
+                item.active = False
+
         elif r.status_code == 304:
-            item.active = True
+            #item.active = True
+            None
 
         else:
             item.active = False
@@ -154,3 +179,69 @@ def process_url(url=None, url_id=None, cache_root=''):
         DBSession.flush()
         return URLSchema().dump(item).data
 
+def add_cia(url, cache_path, archive_path=None):
+    if archive_path:
+        filename=os.path.join(cache_path,'archive_root',archive_path)
+    else:
+        filename=os.path.join(cache_path,url.filename)
+
+    new = False
+    cia = DBSession.query(CIA).filter_by(url_id=url.id,path=archive_path).first()
+    if not cia:
+        new = True
+        cia = CIA(active=False)
+
+    cia.path = archive_path
+    cia.version = url.version
+    cia.size = os.path.getsize(filename)
+    cia.mtime = datetime.fromtimestamp(os.path.getmtime(filename))
+    cia.sha256 = checksum_sha256(filename)
+
+    with open(filename, 'rb') as f:
+        f.seek(11292)
+        try:
+            cia.titleid = "%0.16X" % numpy.fromfile(f, dtype='>u8', count=1)[0]
+        except IndexError:
+            return None
+
+        if cia.titleid[:8] == "00040000": # and data['titleid'][0:8] != "00048004":
+            cia.active = True
+
+        f.seek(-14016, 2)
+        (cia.name_s,cia.name_l,cia.publisher,cia.icon_s,cia.icon_l) = decode_smdh(f.read(14016))
+
+    if cia.active:
+        DBSession.add(url)
+        DBSession.flush()
+        cia.url_id = url.id
+        DBSession.add(cia)
+        DBSession.flush()
+        return(True)
+
+def add_tdsx(url_entry, cache_path, archive_path=None):
+    None
+
+def add_smdh(url_entry, cache_path, archive_path=None):
+    None
+
+def add_arm9(url_entry, cache_path, archive_path=None):
+    None
+
+def add_xml(url_entry, cache_path, archive_path=None):
+    None
+
+def decode_smdh(smdh):
+        # freeShop doesn't have SMDH magic. WTF?
+        #if req.content[0:4] != 'SMDH':
+        #               return None
+
+        # The english description starts at SMDH offset 520, encoded UTF-16
+        name_s = smdh[520:520+128].decode('utf-16').rstrip('\0')
+        name_l = smdh[520+128:520+384].decode('utf-16').rstrip('\0')
+        publisher = smdh[520+384:520+512].decode('utf-16').rstrip('\0')
+
+        # These are the SMDH icons, both small and large.
+        icon_s = base64.b64encode(smdh[8256:8256+1152])
+        icon_l = base64.b64encode(smdh[9408:9408+4608])
+
+        return (name_s,name_l,publisher,icon_s,icon_l)
