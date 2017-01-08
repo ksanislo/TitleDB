56a7c230cf4a43af4adfd2a8883a173e4bf36ece
Ken Sanislo <ken@intherack.com>
2017-01-07 21:51:36 -0800

    fix cron transactions

--- titledb/cron.py
+++ titledb/cron.py
@@ -8,19 +8,15 @@ import json
 import transaction
 
 def process_submission_queue(cache_root=None):
-    submissions = DBSession.query(Submission).filter_by(active=True).all()
-    for submission in submissions:
-        if submission.url:
-            try:
-                with transaction.manager:
-                #with DBSession.begin_nested():
+    with transaction.manager:
+        submissions = DBSession.query(Submission).filter_by(active=True).all()
+        for submission in submissions:
+            if submission.url:
+                try:
                     result = json.dumps(process_url(submission.url, cache_root=cache_root))
-                    #process_url(asset['browser_download_url'], cache_root=cache_root)
-            except:
-                DBSession.rollback()
-            else:
-                submission.status = result            
-                submission.active = False
-                transaction.commit()
-                #DBSession.flush()
+                except:
+                    DBSession.rollback()
 
+            submission.status = result            
+            submission.active = False
+    
