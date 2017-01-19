16ce9a534c5241afa5cc38f649611d64bd1640a3
Ken Sanislo <ken@intherack.com>
2017-01-18 19:52:12 -0800

    cleaner mod permissions

--- titledb/views.py
+++ titledb/views.py
@@ -139,14 +139,9 @@ class BaseView(object):
                 self.active_schema = self.moderator_cls()
             elif not self.moderator_cls and self.request.method == 'PUT' and 'group:mod' in self.request.effective_principals:
                 all_fields = list(self.schema_cls._declared_fields.keys())
-                try: # Allow moderators to toggle "active" by default
-                    all_fields.remove('active')
-                except ValueError:
-                    None
-                # Also, moderators can edit any linking *_id field
-                all_fields = [x for x in all_fields if not x.endsswith('_id')]
-
-                self.active_schema = self.schema_cls(dump_only=all_fields)
+                # Allow moderators to toggle "active" and edit some *_id fields by default
+                mod_allowed=['active','assets_id','entry_id','smdh_id','xml_id']
+                self.active_schema = self.schema_cls(dump_only=[x for x in all_fields if x not in mod_allowed])
             elif self.everyone_cls and self.request.method == 'POST':
                 self.active_schema = self.everyone_cls()
             else: # No preferred schema found, so set everything as dump_only
